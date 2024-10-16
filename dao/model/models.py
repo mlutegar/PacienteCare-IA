cec06baecb2c36142543e306cf41bb6042de4a5f
mlutegar <mlutegar@gmail.com>
2024-10-16 16:11:37 -0300

    feat: ajustando erro do chroma

--- dao/model/models.py
+++ dao/model/models.py
@@ -1,13 +1,13 @@
+import os
 from pydantic import BaseModel
 from chromadb.config import DEFAULT_TENANT, DEFAULT_DATABASE, Settings
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 import chromadb
-import os
 
 # Configurações de ambiente
-os.environ["OPENAI_API_KEY"] = ("-")
-os.environ["CHROMA_PATH"] = "/chroma"
+os.environ["OPENAI_API_KEY"] = "-"
+os.environ["CHROMA_PATH"] = "/home/runner/work/chroma_data"
 os.environ["PINECONE_API_KEY"] = "e9cb230e-979e-4c7d-967b-8788ad806bd6"
 os.environ["QDRANT_API_KEY"] = "y51snXPq9TJIC8uYJm5_2quxGdx8miv4loSCcBB1a_k8ke_2y03ZgA"
 
