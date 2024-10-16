23ba17862a61ca27436f877dbe38f999dc8632f9
mlutegar <mlutegar@gmail.com>
2024-10-16 15:30:03 -0300

    feat: adicionando o univorn

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dao.view.routes import router
+import uvicorn
 
 app = FastAPI()
 
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Origem que você deseja permitir
+    allow_origins=["https://mlutegar.github.io"],  # Origem que você deseja permitir
     allow_credentials=True,
     allow_methods=["*"],  # Métodos HTTP permitidos
     allow_headers=["*"],  # Cabeçalhos HTTP permitidos
@@ -15,3 +16,7 @@ app.add_middleware(
 
 # Incluir as rotas
 app.include_router(router)
+
+# Iniciar a aplicação com Uvicorn
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
