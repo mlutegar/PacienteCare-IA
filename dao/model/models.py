eef7c9b10eec0b7593baf1930be11ce609d10047
mlutegar <mlutegar@gmail.com>
2024-10-17 11:29:50 -0300

    feat: adicionando chave

---
+++ dao/model/models.py
@@ -0,0 +1,44 @@
+import os
+from pydantic import BaseModel
+from chromadb.config import DEFAULT_TENANT, DEFAULT_DATABASE, Settings
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+import chromadb
+
+# Configurações de ambiente
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+os.environ["CHROMA_PATH"] = "/home/runner/work/chroma_data"
+os.environ["PINECONE_API_KEY"] = "e9cb230e-979e-4c7d-967b-8788ad806bd6"
+os.environ["QDRANT_API_KEY"] = "y51snXPq9TJIC8uYJm5_2quxGdx8miv4loSCcBB1a_k8ke_2y03ZgA"
+
+if not OPENAI_API_KEY:
+    raise ValueError("A chave da API da OpenAI não está definida. Verifique se a variável de ambiente OPENAI_API_KEY está configurada corretamente.")
+
+# Modelo de dados para o paciente
+class PatientData(BaseModel):
+    idade: int
+    alergias: str
+    procedimentos_anteriores: str
+    medicamentos: str
+    condicoes_saude: str
+    procedimento: str
+    toxina: str
+
+# Função de embeddings
+def get_embedding_function():
+    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+    return embeddings
+
+# Configuração do cliente ChromaDB
+client = chromadb.PersistentClient(
+    path=os.environ['CHROMA_PATH'],
+    settings=Settings(),
+    tenant=DEFAULT_TENANT,
+    database=DEFAULT_DATABASE,
+)
+
+# Instância do banco de dados ChromaDB
+db = Chroma(
+    client=client, collection_name="iamed",
+    persist_directory=os.environ['CHROMA_PATH'], embedding_function=get_embedding_function()
+)
